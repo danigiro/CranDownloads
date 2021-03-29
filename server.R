6295d388909d26a6b5fef54eb78653c81e7816e5
dani <girolimettodaniele@gmail.com>
2021-03-29 12:03:54 +0200

    NULL issues

--- server.R
+++ server.R
@@ -19,35 +19,38 @@ g_legend <- function(a.gplot){
 
 shinyServer(function(input, output) {
   downloads <- reactive({
-      packages <- input$package
-      date_list = structure(c(sapply(packages, function(x){
-        pkg_data = content(GET(paste0("http://crandb.r-pkg.org/", x, "/all")))$timeline[[1]]
-        as.Date(pkg_data)}), Sys.Date() -1), class = "Date")
-      
-      cran_downloads0 <- purrr::possibly(cran_downloads, NULL, quiet = TRUE)
-      d <- cran_downloads0(package = packages, 
-                      from    = min(date_list), 
-                      to      = Sys.Date()-1)
-      if (input$transformation=="weekly") {
-        d <- d %>%
-          group_by(package, week(date), year(date)) %>%
-          mutate(count=sum(count), date = max(date)) %>% 
-          unique() %>% ungroup()
-      } else if (input$transformation=="monthly") {
-        d <- d %>%
-          group_by(package, month(date), year(date)) %>%
-          mutate(count=sum(count), date = max(date)) %>% 
-          unique() %>% ungroup()
-      } else if (input$transformation=="cumulative") {
-        d <- d %>%
-          group_by(package) %>%
-          transmute(count=cumsum(count), date=date)  %>% 
-          ungroup()
-      }
-      d
-  })
+    packages <- input$package
+    date_list = structure(c(sapply(packages, function(x){
+      pkg_data = content(GET(paste0("http://crandb.r-pkg.org/", x, "/all")))$timeline[[1]]
+      as.Date(pkg_data)}), Sys.Date() -1), class = "Date")
     
+    cran_downloads0 <- purrr::possibly(cran_downloads, NULL, quiet = TRUE)
+    d <- cran_downloads0(package = packages, 
+                         from    = min(date_list), 
+                         to      = Sys.Date()-1)
+    if (input$transformation=="weekly") {
+      d <- d %>%
+        group_by(package, week(date), year(date)) %>%
+        mutate(count=sum(count), date = max(date)) %>% 
+        unique() %>% ungroup()
+    } else if (input$transformation=="monthly") {
+      d <- d %>%
+        group_by(package, month(date), year(date)) %>%
+        mutate(count=sum(count), date = max(date)) %>% 
+        unique() %>% ungroup()
+    } else if (input$transformation=="cumulative") {
+      d <- d %>%
+        group_by(package) %>%
+        transmute(count=cumsum(count), date=date)  %>% 
+        ungroup()
+    }
+    d
+  })
+  
   output$downloadsPlot <- renderPlot({
+    if(is.null(input$package)){
+      ggplot() +theme_minimal() 
+    }else{
       d <- downloads()
       
       g1 <- ggplot(d[between(d$date, max(d$date) - years(1), max(d$date)),], 
@@ -76,51 +79,56 @@ shinyServer(function(input, output) {
         theme(legend.position = "bottom")
       
       grid.arrange(arrangeGrob(arrangeGrob(g1, g12, ncol = 2),
-        g2 + theme(legend.position = "none"), bottom = "Date", left = "Number of downloads"),
-        g_legend(g2), heights = c(2,0.25))
+                               g2 + theme(legend.position = "none"), bottom = "Date", left = "Number of downloads"),
+                   g_legend(g2), heights = c(2,0.25))
+    }
   })
   
   output$graph <- renderPlotly({
-    d <- downloads()
-    plot_ly(d, x = ~date, y = ~count, color = ~package, 
-            colors = hue_pal()(length(unique(d$package))), 
-            type="scatter", mode = "markers+lines", size = I(2),
-            line = list(shape = 'linear', width = 0.35, dash = 'dot')) %>%
-      layout(
-        xaxis = list(
-          title = "Date",
-          range=c(min(d$date),max(d$date)),
-          constrain = "range",
-          rangeselector = list(
-            buttons = list(
-              list(
-                count = 3,
-                label = "3 mo",
-                step = "month",
-                stepmode = "backward"),
-              list(
-                count = 6,
-                label = "6 mo",
-                step = "month",
-                stepmode = "backward"),
-              list(
-                count = 1,
-                label = "1 yr",
-                step = "year",
-                stepmode = "backward"),
-              list(
-                count = 1,
-                label = "YTD",
-                step = "year",
-                stepmode = "todate"),
-              list(
-                label = 'All',
-                method = "relayout",
-                args = list(list(xaxis = list(range = c(0,10)))))
-            )),
-          
-          rangeslider = list(type = "date",
-                             range=c(min(d$date),max(d$date)))),
-        yaxis = list(title = "Number of downloads", fixedrange = FALSE))
+    if(is.null(input$package)){
+      plot_ly(type="scatter", mode = "markers+lines")
+    }else{
+      d <- downloads()
+      plot_ly(d, x = ~date, y = ~count, color = ~package, 
+              colors = hue_pal()(length(unique(d$package))), 
+              type="scatter", mode = "markers+lines", size = I(2),
+              line = list(shape = 'linear', width = 0.35, dash = 'dot')) %>%
+        layout(
+          xaxis = list(
+            title = "Date",
+            range=c(min(d$date),max(d$date)),
+            constrain = "range",
+            rangeselector = list(
+              buttons = list(
+                list(
+                  count = 3,
+                  label = "3 mo",
+                  step = "month",
+                  stepmode = "backward"),
+                list(
+                  count = 6,
+                  label = "6 mo",
+                  step = "month",
+                  stepmode = "backward"),
+                list(
+                  count = 1,
+                  label = "1 yr",
+                  step = "year",
+                  stepmode = "backward"),
+                list(
+                  count = 1,
+                  label = "YTD",
+                  step = "year",
+                  stepmode = "todate"),
+                list(
+                  label = 'All',
+                  method = "relayout",
+                  args = list(list(xaxis = list(range = c(0,10)))))
+              )),
+            
+            rangeslider = list(type = "date",
+                               range=c(min(d$date),max(d$date)))),
+          yaxis = list(title = "Number of downloads", fixedrange = FALSE))
+    }
   })
 })
--- ui.R
+++ ui.R
@@ -15,19 +15,19 @@ fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(style = "background: white",
-      wellPanel(HTML("Enter an (or multiple) R package(s) to see the number of downloads over time from the RStudio CRAN Mirror."),
-                selectInput("package", 
-                  label = "Packages:",
-                  selected = sample(package_names, 2),
-                  choices = package_names,
-                  multiple = TRUE),   
-      radioButtons("transformation", 
-                   "Data Transformation:",
-                   c("Daily" = "daily","Weekly" = "weekly", "Monthly" = "monthly", "Cumulative" = "cumulative"))),
-      wellPanel(HTML("Created using the <a href='https://github.com/metacran/cranlogs'>cranlogs</a> package",
-                     "and starting from the shiny app described <a href='http://varianceexplained.org/r/cran-view/'>here</a>.",
-                     "This app is not affiliated with RStudio or CRAN.",
-                     "You can find the code for the this app <a href='https://github.com/daniGiro/CranDownloads'>here</a>"))
+                 wellPanel(HTML("Enter an (or multiple) R package(s) to see the number of downloads over time from the RStudio CRAN Mirror."),
+                           selectizeInput("package", 
+                                          label = "Packages:",
+                                          selected = sample(package_names, 2),
+                                          choices = package_names,
+                                          multiple = TRUE),   
+                           radioButtons("transformation", 
+                                        "Data Transformation:",
+                                        c("Daily" = "daily","Weekly" = "weekly", "Monthly" = "monthly", "Cumulative" = "cumulative"))),
+                 wellPanel(HTML("Created using the <a href='https://github.com/metacran/cranlogs'>cranlogs</a> package",
+                                "and starting from the shiny app described <a href='http://varianceexplained.org/r/cran-view/'>here</a>.\n",
+                                "This app is not affiliated with RStudio or CRAN.\n",
+                                "You can find the code for the this app <a href='https://github.com/daniGiro/CranDownloads'>here</a>"))
     ),
     
     # Show a plot of the generated distribution
